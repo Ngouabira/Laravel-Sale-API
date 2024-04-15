a29374ff65163d7b2bd4ef1cf8a24bd9ecfcb6fb
Valdy NGOUABIRA <valdyngouabira@gmail.com>
2024-04-15 23:57:40 +0000

    wip

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SaleController;
@@ -19,7 +20,24 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
+
+Route::get('login', function () {
+    return response()->json(['error' => 'Unauthorized'], 401);
+})->name('login');
+
+Route::middleware('auth:api')->group(function () {
     Route::apiResource('/user', UserController::class);
     Route::apiResource('/category', CategoryController::class);
     Route::apiResource('/product', ProductController::class);
